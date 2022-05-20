27d7cb0c9fde501b7fa4796767c28141e9890172
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-05-20 14:19:32 -0700

    STUFF PART TWO

---
+++ src/main/java/frc/robot/EtherAutoCommand.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.MkSwerveTrain.ETHERAUTO;
+import frc.robot.MkSwerveTrain.ETHERRCW;
+
+public class EtherAutoCommand extends CommandBase {
+  /** Creates a new EtherAuto. */
+  private double totalDistance;
+  private double thetaTurn;
+  private double RCWauto;
+  private ETHERAUTO mode;
+  private ETHERRCW turny;
+  private double turnyAuto;
+  private double distanceA;
+  private double heading;
+  private int side;
+  private MkSwerveTrain train = MkSwerveTrain.getInstance();
+  
+  public EtherAutoCommand(double distanceA, double dist, double ang, int side, double heading)
+  {
+   /* this.RCWauto = RCWauto;
+    this.mode = mode;
+    this.turny = turny;*/
+    this.totalDistance = dist;
+    this.thetaTurn = ang;
+   // this.turnyAuto = turnyAuto;
+   this.distanceA = distanceA;
+    this.heading = heading;
+    this.side = side;
+
+    //want theta turn, want turny auto, dont want rcw, curve and specific
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    train.startDrive();
+    //train.startTrain();
+    //SmartDashboard.putNumber("anglglgl", DISTANGLE.angleuno);
+    //SmartDashboard.putNumber("distt", DISTANGLE.distanceuno);
+    train.setEtherAuto(totalDistance, distanceA);
+    SmartDashboard.putNumber("dist", heading);
+    SmartDashboard.putNumber("calc", side);
+    //SmartDashboard.putBoolean("key", true);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    train.etherAutoUpdate(thetaTurn, heading, side);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //train.stopEverything();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+   // SmartDashboard.putBoolean("false", false);
+    return train.isFinished();
+  }
+}
---
+++ src/main/java/frc/robot/Swrv.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AUTO.DISTANGLE;
+
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Swrv extends SequentialCommandGroup {
+  /** Creates a new Swrv. */
+  public Swrv() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(//deadline(new Turn(((DISTANGLE.angleuno)))).withTimeout(1),
+    
+    //deadline(new Turn(0)).withTimeout(1),
+    deadline(new Turn(0).withTimeout(1)),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)), DISTANGLE.sidePos, DISTANGLE.headinguno)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingdos)).withTimeout(6),
+    
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingtres)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingquad)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingsinco)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingsix)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingsev)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingocto)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingnine)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingele)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingele)).withTimeout(6),
+    deadline(new Turn(0)).withTimeout(1),
+    
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingele)).withTimeout(6),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingtwel)).withTimeout(6),
+
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingthir)).withTimeout(6),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sidePos, DISTANGLE.headingfourt)).withTimeout(6),
+    
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingfif)).withTimeout(6),
+    deadline(new EtherAutoCommand(DISTANGLE.distanceA, DISTANGLE.distance, ((DISTANGLE.angle)),    DISTANGLE.sideCon, DISTANGLE.headingsixt)).withTimeout(6));
+             // deadline(new Stop()));
+  }
+}
