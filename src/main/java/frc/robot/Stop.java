ef7d7910c87f858ae1eeb7ee757e74003ffc7fb9
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-10-31 12:53:22 -0700

    Revert "TRUE FORMATTING"
    This reverts commit 060e961005b600fefbcfe82f3dfe61dad5488581.

--- src/main/java/frc/robot/Stop.java
+++ src/main/java/frc/robot/Stop.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class Stop extends CommandBase {
   /** Creates a new Stop. */
   private MkSwerveTrain train = MkSwerveTrain.getInstance();
-
   public Stop() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -28,8 +27,7 @@ public class Stop extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
